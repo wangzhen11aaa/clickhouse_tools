1b2298f3a1aa0956867e1748f4fce146f7c32e0c
wangzhen05 <wangzhen05@meicai.cn>
2021-04-11 15:23:15 +0800

    local test

--- utils/clickhouse_connector.py
+++ utils/clickhouse_connector.py
@@ -2,6 +2,8 @@
 
 from clickhouse_driver import Client
 from common.mc_logging import mc_logger
+from utils.table_schema_modifier import TableSchemaModifier
+
 """
     This ClickHouseConnector class is responsible for connecting the 
     source clickhouse database and target clickhouse database and migrate the
@@ -55,6 +57,10 @@ class ClickHouseConnector:
         cls.source_ck_port = op.source_ck_port
         cls.target_ck_ip = op.target_ck_ip
         cls.target_ck_port = op.target_ck_port
+        cls.get_source_connection()
+        cls.get_databases()
+        cls.get_target_connection()
+        cls.get_tables_and_split()
 
     @classmethod
     def get_source_connection(cls):
@@ -70,19 +76,24 @@ class ClickHouseConnector:
     @classmethod
     def get_rid_of_tuple_in_list(cls, l):
         l = [item[0] for item in l]
+        return l
 
     @classmethod
     def get_databases(cls):
         cls.source_database_list = cls.client_for_source.execute(cls.SHOW_DATABASE)
-        cls.get_rid_of_tuple_in_list(cls.source_database_list)
-        
+        cls.source_database_list = cls.get_rid_of_tuple_in_list(cls.source_database_list)
+
+    # The tables will be overwritten when this method invokes.
     @classmethod
     def get_tables_and_split(cls, database):
         cls.client_for_source.execute(cls.USE_DATABASE %database)
         cls.table_list = cls.client_for_source.execute(cls.SHOW_TABLES)
-        cls.get_rid_of_tuple_in_list(cls.table_list)
-        cls.local_table_list = [filter(lambda item: ("_local" in item), cls.table_list)]
-        cls.no_local_table_list = [filter(lambda item:("_local" not in item), cls.table_list)]
+        cls.table_list = cls.get_rid_of_tuple_in_list(cls.table_list)
+        mc_logger.info("cls.table_list after get_rid_of_tuple_in_list method : " + str(cls.table_list))
+        cls.local_table_list = filter(lambda item: ("_local" in item), cls.table_list)
+        mc_logger.info("cls.local_table_list after filter method : " + str(cls.local_table_list))
+        cls.no_local_table_list = filter(lambda item:("_local" not in item), cls.table_list)
+        mc_logger.info("cls.no_local_table_list after filter method : " + str(cls.no_local_table_list))
  
     # Return the table schema.
     @classmethod
@@ -92,19 +103,73 @@ class ClickHouseConnector:
 
     @classmethod
     def create_database(cls, database):
-        mc_logger.info("Create database, Ready Go!")
+        mc_logger.info("Create database %s, Ready Go!" %(database))
         cls.client_for_target.execute(cls.CREATE_DATABASE_QUERY %(database))
+
+    @classmethod
+    def create_table(cls, table_schema):
+        mc_logger.info("Create table <table schema %s>, Ready Go !" %(table_schema))
+        cls.client_for_target.execute(table_schema)
+
+    @classmethod
+    def do_create_databases(cls):
+         # Loop each database to create database
+        for database in cls.source_database_list:
+            try:
+                mc_logger.info ("do create databse %s" %(database))
+                cls.create_database(database)
+            except Exception, e:
+                mc_logger.error("Exception when execute create database %s" %(e.message))
+
+    @classmethod
+    def do_create_tables(cls):
+        # Loop each database to create table, First we create local tables
+        for database in cls.source_database_list:
+            if database == "system":
+                continue
+            cls.get_tables_and_split(database)
+            mc_logger.info("local tables %s in %s" %(str(cls.local_table_list), database))
+            # First process local_table
+            for local_table in cls.local_table_list:
+                # Fetch the table schema
+                table_schema = cls.fetch_table_schema(database, local_table)[0][0]
+                mc_logger.info("Table's schema %s" %(table_schema))
+                target_table_schema = TableSchemaModifier.modify(table_schema, database, local_table)
+                try:
+                    mc_logger.info("Will construct table on target cluster")
+                    cls.create_table(target_table_schema)
+                except Exception:
+                    mc_logger.error("Exception when executing create table %s" %(Exception.message))
+            # Create distributed tables
+            mc_logger.info("Will Create distributed Tables in %s" %(database))
+            cls.do_create_distributed_tables(database)
+            
+    @classmethod
+    def do_create_distributed_tables(cls, database):
+        mc_logger.info("distributed tables %s in %s" %(str(cls.no_local_table_list), database))
+        for distributed_table in cls.no_local_table_list:
+          # Fetch the table schema
+          table_schema = cls.fetch_table_schema(database, distributed_table)[0][0]
+          mc_logger.info("Table's schema %s" %(table_schema))
+          target_table_schema = TableSchemaModifier.modify(table_schema, database, distributed_table)
+          try:
+              mc_logger.info("Will construct table on target cluster")
+              cls.create_table(target_table_schema)
+          except Exception:
+              mc_logger.error("Exception when executing create table %s" %(Exception.message))
+   
 
 if __name__ == "__main__":
     ClickHouseConnector.source_ck_ip = "192.168.254.43"
     ClickHouseConnector.source_ck_port = 9001
-    ClickHouseConnector.target_ck_ip = "192.168.254.43"
-    ClickHouseConnector.target_ck_port = 9001
+    ClickHouseConnector.target_ck_ip = "192.168.254.46"
+    ClickHouseConnector.target_ck_port = 9002
     ClickHouseConnector.get_source_connection()
     ClickHouseConnector.get_target_connection()
     ClickHouseConnector.get_databases()
-    ClickHouseConnector.create_database("default")
-    print "database " + str(ClickHouseConnector.source_database_list)
-    print "tables " + str(ClickHouseConnector.table_list)
-    print "table schema " + str(ClickHouseConnector.fetch_table_schema("cloudatlas", "cloudatlas_user_group_local"))
+    ClickHouseConnector.do_create_databases()
+    ClickHouseConnector.do_create_tables()
+    #print "database " + str(ClickHouseConnector.source_database_list)
+    #print "tables " + str(ClickHouseConnector.table_list)
+    #print "table schema " + str(ClickHouseConnector.fetch_table_schema("cloudatlas", "cloudatlas_user_group_local"))
     
