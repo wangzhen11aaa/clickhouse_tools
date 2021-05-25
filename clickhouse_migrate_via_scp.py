a34cd4a2b295247e5dd99bd154f15330b923bf5d
wangzhen05 <wangzhen05@meicai.cn>
2021-05-25 16:49:12 +0800

    单线程版本成功

--- clickhouse_migrate_via_scp.py
+++ clickhouse_migrate_via_scp.py
@@ -3,6 +3,8 @@ import sys
 import subprocess
 import logging
 
+#项目以root用户执行
+
 logging.basicConfig(filename="migrate_via_scp.log", filemode="w", level=logging.INFO)
 
 dataPath = "/data/d1/clickhouse/data"
@@ -57,16 +59,29 @@ class PartitionSelector(object):
 
 class InitEnv(object):
     cleanLocalEnvCMD = "sudo su - root -c 'rm -rf /data/d1/migrate_from'"
+    """
+       cleanLocalEnvCMD = "sudo su - root -c 'rm -rf /data/d1/migrate_from'"
+    """
     createLocalEnvCMD = "sudo su - root -c 'mkdir -p  /data/d1/migrate_from && chown -R op_admin:op_admin /data/d1/migrate_from'"
+    """
+        initLocalEnvCMD = "sudo su - root -c 'mkdir -p  /data/d1/migrate_from && chown -R op_admin:op_admin /data/d1/migrate_from'"
 
+    """
     # %s 是ip的 占位符
     cleanRemoteEnvCMD = "ssh -p 50022 op_admin@%s sudo su - root -c \\'rm -rf /data/d1/migrate_in\\'"
+    """
+        cleanRemoteEnvCMD = "ssh -p 50022 op_admin@10.12.1.11 sudo su - root -c \\'rm -rf /data/d1/migrate_in\\'"
+    """
     # 创建migrate_in目录
-    createRemoteEnvCMD = "ssh -p 50022 op_admin@%s sudo su - root -c \\'mkdir -p /data/d1/migrate_in\\'"
-    """ssh -p 50022 op_admin@10.12.1.11 sudo su - root -c \\'chown -R op_admin:op_admin /data/d1/migrate_in\\'"""
+    initRemoteEnvCMD = "ssh -p 50022 op_admin@%s sudo su - root -c \\'mkdir -p /data/d1/migrate_in\\'"
+    """
+        initRemoteEnvCMD = "ssh -p 50022 op_admin@10.12.1.11 sudo su - root -c \\'mkdir -p /data/d1/migrate_in\\' "
+    """
     # 改变migrate_in的owner
     remoteChownCMD = "ssh -p 50022 op_admin@%s sudo su - root -c \\'chown -R op_admin:op_admin /data/d1/migrate_in\\'"
-
+    """
+        remoteChownCMD = "ssh -p 50022 op_admin@10.12.1.11 sudo su - root -c \\'chown -R op_admin:op_admin /data/d1/migrate_in\\'"
+    """
     @classmethod
     def initLocalEnv(cls):
         out, err = subprocess.Popen(cls.cleanLocalEnvCMD,shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -81,11 +96,11 @@ class InitEnv(object):
         out, err = subprocess.Popen(cls.cleanRemoteEnvCMD %(ip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
         print("cleanRemoteEnvCMD stdout %s : " %(cls.cleanRemoteEnvCMD %(ip)) + out.decode('utf-8') + " stderr: " + err.decode('utf-8'))
         logging.info("cleanRemoteEnvCMD stdout %s : " %(cls.cleanRemoteEnvCMD %(ip)) + out.decode('utf-8') + " stderr: " + err.decode('utf-8'))
-        
-        out, err = subprocess.Popen(cls.createRemoteEnvCMD %(ip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-        print("createRemoteEnvCMD stdout %s : " %(cls.createRemoteEnvCMD %(ip)) + out.decode('utf-8') + " stderr: " + err.decode('utf-8'))
-        logging.info("createRemoteEnvCMD stdout %s : " %(cls.createRemoteEnvCMD %(ip)) + out.decode('utf-8') + " stderr: " + err.decode('utf-8'))
-        
+
+        out, err = subprocess.Popen(cls.initRemoteEnvCMD %(ip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        print("initRemoteEnvCMD stdout %s : " %(cls.initRemoteEnvCMD %(ip)) + out.decode('utf-8') + " stderr: " + err.decode('utf-8'))
+        logging.info("initRemoteEnvCMD stdout %s : " %(cls.initRemoteEnvCMD %(ip)) + out.decode('utf-8') + " stderr: " + err.decode('utf-8'))
+
         out, err = subprocess.Popen(cls.remoteChownCMD %(ip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
         print("remoteChownCMD stdout %s : " %(cls.remoteChownCMD %(ip)) + out.decode('utf-8') + " stderr: " + err.decode('utf-8'))
         logging.info("remoteChownCMD stdout %s : " %(cls.remoteChownCMD %(ip)) + out.decode('utf-8') + " stderr: " + err.decode('utf-8'))
@@ -98,9 +113,12 @@ class Compressor(object):
     # 跳转到目录,为了控制压缩路径为./,压缩然后mv
     compressAndMoveCMD = "cd %s && touch %s.tar.gz && tar --exclude=%s.tar.gz --exclude=format_version.txt -czvf %s.tar.gz . && mv -f %s.tar.gz /data/d1/migrate_from"
     # 测试
-    """compressAndMoveCMD = "cd /data/d1/clickhouse/data/etl/etl_app_bicore_cityline_classmmc_wma_profit_1d_local && \
-        touch etl_app_bicore_cityline_classmmc_wma_profit_1d_local.tar.gz && tar --exclude=etl_app_bicore_cityline_classmmc_wma_profit_1d_local.tar.gz \
-        --exclude=format_version.txt -czf etl_app_bicore_cityline_classmmc_wma_profit_1d_local.tar.gz . && mv -f etl_app_bicore_cityline_classmmc_wma_profit_1d_local.tar.gz /data/d1/migrate_from"""
+    """
+        compressAndMoveCMD = "cd /data/d1/clickhouse/data/app/cube_city_class1_mmc_retail_sale_1d_local && \
+        touch cube_city_class1_mmc_retail_sale_1d_local.tar.gz && tar --exclude=cube_city_class1_mmc_retail_sale_1d_local.tar.gz \
+        --exclude=format_version.txt -czf cube_city_class1_mmc_retail_sale_1d_local.tar.gz . && mv -f cube_city_class1_mmc_retail_sale_1d_local.tar.gz /data/d1/migrate_from"
+
+        """
 
     #传入数据路径，进行压缩
     @classmethod
@@ -141,7 +159,7 @@ class Transfer(object):
     transferCMD="cd %s && scp -P 50022 %s op_admin@%s:%s"
     """
         transferCMD = "cd /data/d1/migrate_from && scp -P 50022 \
-            etl_app_bicore_cityline_classmmc_wma_profit_1d_local.tar op_admin@10.12.1.11:/data/d1/migrate_in/"
+            cube_city_class1_mmc_retail_sale_1d_local.tar.gz op_admin@10.12.1.11:/data/d1/migrate_in/"
     """
     @classmethod
     def transferTar(cls, tarFilePath, ip, targetDataPath="/data/d1/migrate_in"):
@@ -153,32 +171,32 @@ class Transfer(object):
     数据传输过去以后，我们需要移动到对应的detached的目录，然后修改tar权限，解压，然后进行attach.
     这几步操作都需要走ssh,进行远程控制操作
 """
-class Move2Detached(object):
-    moveCMD="ssh -p 50022 op_admin@%s sudo su - root -c \\' mv %s/%s %s/%s/%s/detached \\' "
+class RemoteMove2Detached(object):
+    moveCMD="ssh -p 50022 op_admin@%s sudo su - root -c \\' mv %s/%s %s/%s/%s/detached\\' "
     """
         moveCMD="ssh -p 50022 op_admin@10.12.1.11 sudo su - \
-            root -c \\' mv /data/d1/migrate_in/etl_app_bicore_cityline_classmmc_wma_profit_1d_local.tar /data/d1/clickhouse/data/etl/etl_app_bicore_cityline_classmmc_wma_profit_1d_local/detached \\' "
+            root -c \\'mv /data/d1/migrate_in/cube_city_class1_mmc_retail_sale_1d_local.tar.gz /data/d1/clickhouse/data/app/cube_city_class1_mmc_retail_sale_1d_local/detached\\'"
     """
     @classmethod
-    def doMove2Detached(cls, ip, databaseName, tableName, targetDataPath="/data/d1/migrate_in"):
-        doMove2DetachedResult = subprocess.Popen(cls.moveCMD %(ip,targetDataPath, tableName+".tar.gz", dataPath, databaseName, tableName), \
+    def move2Detached(cls, ip, databaseName, tableName, targetDataPath="/data/d1/migrate_in"):
+        moveCMDResult = subprocess.Popen(cls.moveCMD %(ip,targetDataPath, tableName+".tar.gz", dataPath, databaseName, tableName), \
             shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
-        logging.info("moveCMD CMDResult: %s " %(cls.transferCMD %(ip, targetDataPath, tableName+".tar.gz", dataPath, databaseName, tableName)) + transferCMDResult.decode('utf-8'))
+        logging.info("moveCMD CMDResult: %s " %(cls.moveCMD %(ip, targetDataPath, tableName+".tar.gz", dataPath, databaseName, tableName)) + moveCMDResult.decode('utf-8'))
 
 """
     进行解压
 """
-class Extract(object):
+class RemoteExtract(object):
     chownCMD = "ssh -p 50022 op_admin@%s sudo su - root -c \\'chown clickhouse:clickhouse %s \\'"
     """
         chownCMD="ssh -p 50022 op_admin@10.12.1.11 sudo su - root -c \\' chown clickhouse:clickhouse \
-            /data/d1/clickhouse/data/etl/etl_app_bicore_cityline_classmmc_wma_profit_1d_local/detached/etl_app_bicore_cityline_classmmc_wma_profit_1d_local.tar \\'"
+            /data/d1/clickhouse/data/app/cube_city_class1_mmc_retail_sale_1d_local/detached/cube_city_class1_mmc_retail_sale_1d_local.tar \\'"
     """
     extractCMD="ssh -p 50022 op_admin@%s sudo su - root -c \\' tar xf %s \\' "
     """
         extractCMD = "ssh -p 50022 op_admin@10.12.1.11 sudo su - root -c \\' tar xf \
-           /data/d1/clickhouse/data/etl/etl_app_bicore_cityline_classmmc_wma_profit_1d_local/detached/etl_app_bicore_cityline_classmmc_wma_profit_1d_local.tar -C \
-              /data/d1/clickhouse/data/etl/etl_app_bicore_cityline_classmmc_wma_profit_1d_local/detached/ \\'"
+           /data/d1/clickhouse/data/app/cube_city_class1_mmc_retail_sale_1d_local/detached/cube_city_class1_mmc_retail_sale_1d_local.tar.gz -C \
+              /data/d1/clickhouse/data/app/cube_city_class1_mmc_retail_sale_1d_local/detached/ \\'"
     """
     @classmethod
     def chownAndExtract(cls, ip, databaseName, tableName):
@@ -187,24 +205,36 @@ class Extract(object):
             shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
         logging.info("chownCMD : chownCMDResult %s" %(cls.chownCMD %(ip, targetTarPath) + chownCMDResult.decode('utf-8')))
 
-        etractCMDResult = subprocess.Popen(cls.extractCMD %(ip, targetTarPath), \
+        extractCMDResult = subprocess.Popen(cls.extractCMD %(ip, targetTarPath), \
             shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
         logging.info("extractCMD extractCMDResult: %s " %(cls.extractCMD %(ip, targetTarPath) + extractCMDResult.decode('utf-8')))
-                     
+
 """
     最后一步，进行Attach工作
 """
-class Attach(object):
-    AttachCMD="ssh -p 50022 op_admin@%s sudo su - root -c \\'bash /data/service/load_data.sh %s %s\\' "
+class RemoteAttach(object):
+    attachCMD="ssh -p 50022 op_admin@%s sudo su - root -c \\'bash /data/service/load_data.sh %s %s\\' "
     """
-        AttachCMD="ssh -p 50022 op_admin@10.12.1.11 sudo su - root -c \\'bash /data/service/load_data.sh etl etl_app_bicore_cityline_classmmc_wma_profit_1d_local \\'"
+        attachCMD="ssh -p 50022 op_admin@10.12.1.11 sudo su - root -c \\'bash /data/service/load_data.sh app cube_city_class1_mmc_retail_sale_1d_local \\'"
+    """
+    rmTarCMD = "ssh -p 50022 op_admin@%s sudo su - root -c \\rm -f %s"
+    """
+        rmTarCMD = "ssh -p 50022 op_admin@10.12.1.11 sudo su - root -c \\' rm -f \
+            /data/d1/clickhouse/data/app/cube_city_class1_mmc_retail_sale_1d_local/detached/cube_city_class1_mmc_retail_sale_1d_local.tar.gz\\'"
     """
     @classmethod
-    def doAttach(cls, ip, databaseName, tableName):
-        attachCMDResult = subprocess.Popen(cls.AttachCMD %(ip, databaseName, tableName), \
+    def attachAndRemove(cls, ip, databaseName, tableName):
+        attachCMDResult = subprocess.Popen(cls.attachCMD %(ip, databaseName, tableName), \
             shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
-        logging.info("attachCMD : attachCMDResult %s" %(cls.AttachCMD %(ip, databaseName, tableName) + attachCMDResult.decode('utf-8')))
-       
+        logging.info("attachCMD : attachCMDResult %s" %(cls.attachCMD %(ip, databaseName, tableName) + attachCMDResult.decode('utf-8')))
+
+        targetTarPath = dataPath+"/"+databaseName+"/"+tableName+"/detached/"+tableName+".tar.gz"
+        rmCMDResult = subprocess.Popen(cls.rmTarCMD %(ip, targetTarPath), \
+            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+        logging.info("rmTarCMD : rmTarCMDResult %s" %(cls.rmTarCMD %(ip, targetTarPath) + rmCMDResult.decode('utf-8')))
+
+
+
 
 
 if __name__ == "__main__":
@@ -218,7 +248,7 @@ if __name__ == "__main__":
     # 初始化当前环境
     InitEnv.initLocalEnv()
     InitEnv.initRemoteEnv("10.12.1.11")
-
+    ip="10.12.1.11"
     #测试DataBaseSelector
     r1 = DataBaseSelector.getDatabaseNames()
     print(repr(r1))
@@ -230,6 +260,8 @@ if __name__ == "__main__":
         for _table in d[_database]:
             Compressor.compress(_database, _table)
             _tarPath = migrateFromPath+"/"+_table+".tar.gz"
-            Md5Sum.localMD5Sum(_tarPath)
-            Transfer.transferTar(_tarPath, '10.12.1.11')
+            Transfer.transferTar(_tarPath, ip)
+            RemoteMove2Detached.move2Detached(ip, _database, _table)
+            RemoteExtract.chownAndExtract(ip, _database, _table)
+            RemoteAttach.attachAndRemove(ip, _database, _table)
         break
